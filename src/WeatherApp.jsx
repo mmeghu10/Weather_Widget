7ef0b71dbceb007ac3a09771f510d8e979e5f835
Meghna <147751588+mmeghu10@users.noreply.github.com>
2024-10-26 19:57:52 +0530

    Add files via upload

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,29 @@
+
+import SearchBox from "./SearchBox";
+import InfoBox from "./InfoBox";
+import { useState } from "react";
+
+
+export default function WeatherApp() {
+    const [weatherInfo, setweatherInfo] = useState({
+        city: "Wonderland",
+        feelslike: 24.84,
+        temp: 25.05,
+        tempMin: 25.05, 
+        tempMax: 25.05,
+        humidity: 47,
+        weather: "haze"
+    });
+
+    let updateInfo = (newInfo) => {
+        setweatherInfo(newInfo);
+    };
+
+    return (
+        <div style={{ textAlign: "center" }} >
+            <h2>Weather App </h2>
+            <SearchBox updateInfo = {updateInfo} />
+            <InfoBox info={weatherInfo} />
+        </div>
+    )
+}
